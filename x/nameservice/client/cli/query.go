d55e5b44193ae70eaf58b28116a7ba64f9210617
jerryma0912 <jerryma0912.student@sina.com>
2019-07-11 23:59:30 +0800

    增加注释

--- x/nameservice/client/cli/query.go
+++ x/nameservice/client/cli/query.go
@@ -39,6 +39,12 @@ func GetCmdResolveName(queryRoute string, cdc *codec.Codec) *cobra.Command {
 			cliCtx := context.NewCLIContext().WithCodec(cdc)
 			name := args[0]
 
+			// cliCtx.QueryWithData()`函数所需的`path`直接从你的查询路径中映射。
+			//- 路径的第一部分用于区分 SDK 应用程序可能的querier类型：`custom`用于`Querier`
+			//- 第二部分（`nameservice`）是将查询路由到的模块的名称。
+			//- 最后是要调用模块中的特定的querier。
+			//- 在这个例子中，第四部分是查询。这是因为查询参数是一个简单的字符串。要启用更复杂的查询输入，你需要使用[`.QueryWithData()`]
+			//- 函数的第二个参数来传入`data`。有关此示例，请参阅 [Staking 模块中的 queriers]
 			res, _, err := cliCtx.QueryWithData(fmt.Sprintf("custom/%s/resolve/%s", queryRoute, name), nil)
 			if err != nil {
 				fmt.Printf("could not resolve name - %s \n", name)
